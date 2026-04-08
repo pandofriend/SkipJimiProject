a137c4053a1b9b6ebbe7a79a27d4adb08705624e
pandofriend <pandaponchos@gmail.com>
2026-04-08 12:18:41 -0400

    Dash Implementation V1
    Added functional Dashing and removed some unnecessary things from when I was testing

--- Assets/UnityTechnologies/Playground/Scripts/Gameplay/ObjectShooter.cs
+++ Assets/UnityTechnologies/Playground/Scripts/Gameplay/ObjectShooter.cs
@@ -15,13 +15,9 @@ public class ObjectShooter : MonoBehaviour
 
     private float timeOfLastSpawn;
     private PlayerControls controls;
-    private bool canDash = true;
-    private bool isDashing;
-    private float dashingPower = 24f;
-    private float dashingTime = 0.2f;
-    private float dashingCooldown = 1f;
+  
 
-    [SerializeField] private TrailRenderer tr;
+    
     private void Awake() => controls = new PlayerControls();
 
     private void OnEnable()
@@ -61,18 +57,5 @@ public class ObjectShooter : MonoBehaviour
         Rigidbody2D rb2D = newObject.GetComponent<Rigidbody2D>();
         if(rb2D != null) rb2D.AddForce(actualDir * shootSpeed, ForceMode2D.Impulse);
     }
-    private IEnumerator Dash()
-    {
-        canDash = false;
-        isDashing = true;
-        float originalGravity = rb.gravityScale;
-        rb.gravityScale = 0f;
-        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
-        tr.emitting = true;
-        yield return new WaitForSeconds(dashingTime);
-        rb.gravityScale = originalGravity;
-        isDashing = false;
-        yield return new WaitForSeconds(dashingCooldown);
-        canDash = true;
-    }
+   
 }
